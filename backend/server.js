eca69c56890f04d8576c85293ca2b0a297ffc782
Dariusz Biela <darek10121998db@gmail.com>
2020-02-29 17:40:16 +0100

    database work!!!!!!

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,16 @@ var cors = require('cors');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var database = require('./controllers/database/api')
+
 app.use(cors());
 
+
+database.connect().then(function(ms){
+    console.log(ms)
+}).catch(function(ms){
+    console.log(ms)
+})
 
 var mainRoutes = require("./controllers/router/api");
 app.use('/', mainRoutes);
@@ -22,3 +30,12 @@ app.use('/', mainRoutes);
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT);
 })
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://adminin:adminin@cluster0-qw89z.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
